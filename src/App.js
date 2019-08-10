547e15f3eae2d7030e6f9a01401288b7976923a3
Derek W <41767875+dwilli77@users.noreply.github.com>
2019-08-09 23:45:37 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Confetti from "react-confetti";
+import { sightWords } from "./words";
+import "./reset.scss";
+import "./App.scss";
 
-function App() {
+const App = () => {
+  const [words, setWords] = useState(sightWords);
+  const [currentWord, setCurrentWord] = useState("");
+  const [typedWord, setTypedWord] = useState("");
+  const [modalOpen, setModalOpen] = useState(false);
+  const [failedValidation, setFailedValidation] = useState(false);
+
+  useEffect(() => {
+    _getWord();
+  }, []);
+
+  // useEffect(() => console.log(words));
+
+  const _getWord = () => {
+    let index = Math.floor(Math.random() * words.length);
+    setCurrentWord(words[index]);
+    setWords(words.filter(word => word !== currentWord));
+  };
+
+  const _checkWord = e => {
+    e.preventDefault();
+    if (currentWord === typedWord.toLowerCase()) {
+      setModalOpen(true);
+      setTimeout(_closeModal, 4000);
+      setTypedWord("");
+      _getWord();
+    } else {
+      setFailedValidation(true);
+      setTimeout(_closeModal, 4000);
+      setTypedWord("");
+    }
+  };
+
+  const _closeModal = () => {
+    setModalOpen(false);
+    setFailedValidation(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {modalOpen ? (
+        <>
+          <div className="grey-out" />
+          <div className="congrats">
+            <Confetti width={400} height={400} />
+            <i className="fas fa-check-circle good" />
+          </div>
+        </>
+      ) : null}
+      {failedValidation ? (
+        <>
+          <div className="grey-out" />
+          <div className="congrats">
+            <i className="fas fa-times-circle bad" />
+          </div>
+        </>
+      ) : null}
+      <div className="app-container">
+        <p className="prompt">Your word is:</p>
+        <h3 className={failedValidation ? "red" : ""}>
+          {currentWord === "i" ? currentWord.toUpperCase() : currentWord}
+        </h3>
+        <form>
+          <input
+            type="text"
+            value={typedWord}
+            onChange={e => setTypedWord(e.target.value)}
+            placeholder="type your word here..."
+            autoComplete="new-word"
+            disabled={modalOpen}
+          />
+          <button className="check-word" onClick={_checkWord}>
+            Check my word
+          </button>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/words.js
@@ -0,0 +1,12 @@
+export const sightWords = [
+  "i",
+  "can",
+  "see",
+  "the",
+  "a",
+  "at",
+  "and",
+  "like",
+  "me",
+  "we"
+];
