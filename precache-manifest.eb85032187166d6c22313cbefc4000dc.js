5448529835442ad75b3ff962d793cd4e21628b58
Derek W <41767875+dwilli77@users.noreply.github.com>
2019-08-09 23:56:30 -0400

    Updates

---
+++ precache-manifest.eb85032187166d6c22313cbefc4000dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5526eaa548636909a095dd177310b9a",
+    "url": "/sight-words/index.html"
+  },
+  {
+    "revision": "c06eaf264a23045ae0fa",
+    "url": "/sight-words/static/css/main.41581f66.chunk.css"
+  },
+  {
+    "revision": "02a5f5082d17391009be",
+    "url": "/sight-words/static/js/2.c243ac47.chunk.js"
+  },
+  {
+    "revision": "c06eaf264a23045ae0fa",
+    "url": "/sight-words/static/js/main.f2905ebf.chunk.js"
+  },
+  {
+    "revision": "5610b6fd800738c4dc3f",
+    "url": "/sight-words/static/js/runtime~main.94f54f0a.js"
+  }
+]);
